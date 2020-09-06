319294ec14552bd7e9acf4c460c5dae844e797ac
Idrissa Ndiouck <wduunx7@gmail.com>
2020-09-06 20:53:49 +0000

    Projet terminé

--- app/src/main/java/com/idyndiouck/gads_leaderbord2020/MainActivity.java
+++ app/src/main/java/com/idyndiouck/gads_leaderbord2020/MainActivity.java
@@ -1,8 +1,18 @@
 package com.idyndiouck.gads_leaderbord2020;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.idyndiouck.gads_leaderbord2020.adapter.MainPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(new MainPagerAdapter(this));
+        new TabLayoutMediator(tabLayout, viewPager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        switch (position) {
+                            case 0:
+                                tab.setText(R.string.learning_leaders);
+                                break;
+                            case 1:
+                                tab.setText(R.string.skill_iq_leaders);
+                        }
+                    }
+                }
+        ).attach();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        menu.findItem(R.id.menu_submit)
+                .getActionView()
+                .findViewById(R.id.btn)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(MainActivity.this, SubmitActivity.class));
+                    }
+                });
+
+        return true;
     }
 }
