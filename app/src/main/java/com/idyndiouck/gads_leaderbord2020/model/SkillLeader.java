781748c0e0fcdfb4d3724252b39e4795141c51fa
AbdElraouf Sabri <abdelraoufsabri@gmail.com>
2020-09-17 13:14:11 +0200

    remove unused functions

--- app/src/main/java/com/idyndiouck/gads_leaderbord2020/model/SkillLeader.java
+++ app/src/main/java/com/idyndiouck/gads_leaderbord2020/model/SkillLeader.java
@@ -15,23 +15,12 @@ public class SkillLeader {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getCountry() {
         return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
     }
 
     public int getScore() {
         return score;
     }
 
-    public void setScore(int score) {
-        this.score = score;
-    }
 }
